d946657aa48af3a72ba9b00aadf9282240680903
mateo2694 <mateosm@outlook.com>
2021-06-11 15:40:09 -0500

    Disabled ads plugin

--- matflix_web_site/src/js/media_player.js
+++ matflix_web_site/src/js/media_player.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
 import Autoplay from './plugins/Autoplay.js';
 import Autopause from './plugins/Autopause.js';
-import AdsPlugin from './plugins/Ads/AdsPlugin.js';
+//import AdsPlugin from './plugins/Ads/AdsPlugin.js';
 // DOM elements
 const video = document.querySelector("video");
 const playButton = document.getElementById("playButton");
 const muteButton = document.getElementById("muteButton");
-const player = new MediaPlayer({ target: video, plugins: [new Autoplay, new Autopause, new AdsPlugin] });
+//const player = new MediaPlayer({ target: video, plugins: [new Autoplay, new Autopause, new AdsPlugin] });
+const player = new MediaPlayer({ target: video, plugins: [new Autoplay, new Autopause] });
 playButton.onclick = () => player.playPause();
 muteButton.onclick = () => player.muteUnmute();
